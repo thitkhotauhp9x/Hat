f2e0f1e681d77a3ab8daf90cf5ea06896701e3a3
manhdt <thitkhotau.no1@gmail.com>
2026-03-10 12:52:19 +0700

    Refactor source code

--- src/hat/assertions/str_format.py
+++
@@ -1,7 +0,0 @@
-import textwrap
-from collections import UserString
-
-
-class StrFormat(UserString):
-    def indent(self, prefix: str) -> str:
-        return textwrap.indent(self.data, prefix, lambda line: True)
